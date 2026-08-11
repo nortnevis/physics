ecc7ae260caf2bcb07944e69aade10354fd94a31
Labmem-015 <naumov.vlad04@gmail.com>
2026-08-11 14:55:18 +0300

    Add ranges calculating in get_max_ndrange

--- snippets/get_max_ndrange.cpp
+++ snippets/get_max_ndrange.cpp
@@ -6,12 +6,14 @@ int main(int, char **) {
     auto wi_count = dev.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
 
     std::println("Max work group size: {}", wg_size);
-    std::println("Max work items number: {}", wi_count);
+    std::println("Max work items number: {}\n", wi_count);
 
-    auto [local, global] = ph::get_task_ranges(dev, {240, 618, 500});
+    std::vector init{240, 618, 1030};
+    auto [local, global] = ph::get_task_ranges(dev, init);
 
+    std::println("Initial sizes: {}", init);
     std::println("Local ranges: {}", local);
-    std::println("Local ranges: {}", local);
+    std::println("Global ranges: {}", global);
 
     return 0;
 }
